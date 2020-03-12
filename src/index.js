61a413e1266b3531704b73a23ba3ead3a6dc9da3
Roy Nyaga <nyagaandreroy@gmail.com>
2020-03-12 02:42:34 +0100

    removed dev-tools package from index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import rootReducer from './redux/reducers/rootReducer'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(rootReducer, composeWithDevTools(
-  applyMiddleware(thunk)
-))
+const store = createStore(rootReducer, applyMiddleware(thunk))
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
 
 serviceWorker.unregister()
