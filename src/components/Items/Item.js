48d0a8671fd603993c8f3bb12a22714d22e7d1b2
Roy Nyaga <nyagaandreroy@gmail.com>
2020-03-16 00:26:49 +0100

    change Item componenet from a class based component to a function basce component

--- src/components/Items/Item.js
+++ src/components/Items/Item.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 
-class Item extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { itemObject } = this.props;
-    return (
-      <div className="col-md-4">
-        <div className="card text-center">
-          <img className="card-img-top" src={itemObject.photo} alt="Card image cap" />
-          <div className="card-body">
-            <h4 className="card-title">{itemObject.name}</h4>
-            <a href={`item/${itemObject.id}`} className="btn btn-secondary">Track Item</a>
-          </div>
+const Item = props => {
+  const { itemObject } = props;
+  return (
+    <div className="col-md-4">
+      <div className="card text-center">
+        <img className="card-img-top" src={itemObject.photo} alt="Card cap" />
+        <div className="card-body">
+          <h4 className="card-title">{itemObject.name}</h4>
+          <a href={`item/${itemObject.id}`} className="btn btn-secondary">Track Item</a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Item.propTypes = {
   itemObject: Proptypes.instanceOf(Object).isRequired,
