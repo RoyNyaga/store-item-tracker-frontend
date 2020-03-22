e8d452d2426c675b94ee39941b55b6df00cf7f05
Roy Nyaga <nyagaandreroy@gmail.com>
2020-03-22 14:44:53 +0100

    fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { persistStore } from 'redux-persist'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { PersistGate } from 'redux-persist/integration/react'
-import * as serviceWorker from './serviceWorker'
-import App from './App'
-import rootReducer from './redux/reducers/rootReducer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { persistStore } from 'redux-persist';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import rootReducer from './redux/reducers/rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
-const persistor = persistStore(store)
+const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
 
-serviceWorker.unregister()
+serviceWorker.unregister();
